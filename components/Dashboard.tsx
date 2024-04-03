891ebdb07120390ac6af6d842a9599589bb33fa6
Martin Fimia <martin.fimia@tui.com>
2024-04-03 18:14:21 +0200

    feat: set up d3 with basic chart

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,6 +1,40 @@
-import 'tailwindcss/tailwind.css'
+import * as d3 from "d3";
+import { FC } from "react";
 
-export const Dashboard = () => {
-  return <div className="border border-blue-500 text-red-500">This is the dashboard :D</div>
+interface IDashboardProps {
+  data: number[]
+  width: number
+  height: number
+  marginTop: number
+  marginRight: number
+  marginBottom: number
+  marginLeft: number
 }
-export default Dashboard;
+
+const Dashboard: FC<IDashboardProps> = ({
+  data = [],
+  width = 640,
+  height = 400,
+  marginTop = 20,
+  marginRight = 20,
+  marginBottom = 20,
+  marginLeft = 20
+}) => {
+  if (!data || !data.length) {
+    return <div>No data to display!</div>
+  }
+
+  const x = d3.scaleLinear([0, data.length - 1], [marginLeft, width - marginRight]);
+  const y = d3.scaleLinear(d3.extent(data) as Iterable<d3.NumberValue>, [height - marginBottom, marginTop]);
+  const line = d3.line((d, i) => x(i), y);
+  return (
+    <svg width={width} height={height}>
+      <path fill="none" stroke="currentColor" strokeWidth="1.5" d={line(data) || undefined} />
+      <g fill="white" stroke="currentColor" strokeWidth="1.5">
+        {data.map((d, i) => (<circle key={i} cx={x(i)} cy={y(d)} r="2.5" />))}
+      </g>
+    </svg>
+  );
+}
+
+export default Dashboard
